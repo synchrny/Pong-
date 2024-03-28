cb95bfe2b4a37284c53b8213e5343d8a210793b5
anotsogoodprogrammer <hamzanawaz17@proton.me>
2024-03-29 12:49:24 +1300

    js file created

---
+++ pong.js
@@ -0,0 +1,12 @@
+//board
+let board;
+let boardWidth = 1000;
+let boardHeight = 1000;
+let context;
+
+window.onload = function() {
+    board = document.getElementById("board");
+    board.height = boardHeight;
+    board.width = boardWidth;
+    context
+}
