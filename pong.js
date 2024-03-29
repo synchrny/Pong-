a9844a55daa1ac0aa361dfc5aeceb3ad9a59887a
anotsogoodprogrammer <hamzanawaz17@proton.me>
2024-03-29 15:02:06 +1300

    fixed some bugs

--- pong.js
+++ pong.js
@@ -1,24 +1,18 @@
 //board
 let board;
-let boardWidth = 900;
-let boardHeight = 900;
-let context;
+let boardWidth = 500;
+let boardHeight = 500;
+let context; 
 
-window.onload = function() {
-    board = document.getElementById("board");
-    board.height = boardHeight;
-    board.width = boardWidth;
-    context = board.getContext("2D"); //used for drawing on the board
-}
-
-//paddles for players
-let playerWidth = 30;
-let playerHeight = 70;
+//players
+let playerWidth = 10;
+let playerHeight = 50;
+let playerVelocityY = 0;
 
 let player1 = {
     x : 10,
     y : boardHeight/2,
-    width: playerHeight,
+    width: playerWidth,
     height: playerHeight,
     velocityY : 0
 }
@@ -26,20 +20,19 @@ let player1 = {
 let player2 = {
     x : boardWidth - playerWidth - 10,
     y : boardHeight/2,
-    width: playerHeight,
+    width: playerWidth,
     height: playerHeight,
     velocityY : 0
 }
 
-
-//ping pong ball
-let ballWidth = 20;
+//ball
+let ballWidth = 10;
 let ballHeight = 10;
 let ball = {
     x : boardWidth/2,
     y : boardHeight/2,
-    width : ballWidth,
-    height : ballHeight,
+    width: ballWidth,
+    height: ballHeight,
     velocityX : 1,
     velocityY : 2
 }
@@ -55,7 +48,7 @@ window.onload = function() {
 
     //draw initial player1
     context.fillStyle="skyblue";
-    context.fillRect(player1.x, player1.y, playerWidth, playerHeight );
+    context.fillRect(player1.x, player1.y, playerWidth, playerHeight);
 
     requestAnimationFrame(update);
     document.addEventListener("keyup", movePlayer);
@@ -65,20 +58,20 @@ function update() {
     requestAnimationFrame(update);
     context.clearRect(0, 0, board.width, board.height);
 
-    //player1
+    // player1
     context.fillStyle = "skyblue";
     let nextPlayer1Y = player1.y + player1.velocityY;
-    if (!outofBounds(nextPlayer1Y)) {
+    if (!outOfBounds(nextPlayer1Y)) {
         player1.y = nextPlayer1Y;
     }
-
+    // player1.y += player1.velocityY;
     context.fillRect(player1.x, player1.y, playerWidth, playerHeight);
+
+    // player2
+    let nextPlayer2Y = player2.y + player2.velocityY;
+    if (!outOfBounds(nextPlayer2Y)) {
+        player2.y = nextPlayer2Y;
+    }
+    // player2.y += player2.velocityY;
+    context.fillRect(player2.x, player2.y, playerWidth, playerHeight);
 }
-
-
-
-
-
-
-
-
